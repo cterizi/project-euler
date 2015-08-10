6b21de6ce6f263aca62edaac40b01f0220d8ad61
cterizi <chryssa.terizi@gmail.com>
2015-08-10 17:20:57 +0300

    Problem 48,add some comments.

--- Problem_48.java
+++ Problem_48.java
@@ -5,14 +5,16 @@ class Problem_48{
     BigInteger finalSum = BigInteger.ZERO;
     BigInteger helpValue = BigInteger.ONE;
     for(int i =1; i<=1000; i++){
-      System.out.println(i);
+      //System.out.println(i);
       sum = helpValue.pow(i);
       helpValue = helpValue.add(BigInteger.ONE);
       finalSum = finalSum.add(sum);
     }
     //String tenString = "10000000000";
     //Error: Convert tenString into BigInteger
-    //BigInteger ten = finalSum % BigInteger("10000000000");
+    //long l = Long.parseLong(tenString);
+    //BigInteger bi = BigInteger(l);
+    //BigInteger ten = finalSum % bi;
     System.out.println("The sum is " + finalSum);
     //System.out.println("The last ten digits are " + ten );
     //The result is 9110846700. 
