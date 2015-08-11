94b654203844dc2d9763354e3dfae342f2b7e327
cterizi <chryssa.terizi@gmail.com>
2015-08-11 19:33:23 +0300

    Delete the variable repeat

--- Problem_28.java
+++ Problem_28.java
@@ -4,14 +4,12 @@ class Problem_28{
     long sum = 1l;
     long temp = 1l;
     long x = 0l;
-    long repeat = 1l; 
     while(temp < Math.pow(size,2) ){
       x = x + 2;
       for(int i = 1; i <= 4; i++){
         temp = temp + x;
         sum = sum + temp;
       } 
-      repeat = repeat + 1;
     }
     System.out.println("The sum is " + sum);
   }
