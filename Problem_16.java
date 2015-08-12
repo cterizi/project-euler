46a0d52e590d2d7ffd98159db1572bb83441d8fc
cterizi <chryssa.terizi@gmail.com>
2015-08-12 14:32:00 +0300

    Initial commit

---
+++ Problem_16.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+
+class Problem_16{
+  public static void main (String[] args){
+    long sum = 0l;;
+    BigInteger base = new BigInteger("2");
+    int power = 1000;
+    BigInteger p ;
+    p = base.pow(power);
+    String word = p.toString();
+    for(char character : word.toCharArray()){
+    sum = sum + Character.getNumericValue(character);;
+    }
+    System.out.println("RESULT: " + sum);
+  }
+}
